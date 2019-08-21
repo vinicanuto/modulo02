ce843ef6d2416915cc3db266340d749a6a727489
Vinicius Canuto <vcanutodev@gmail.com>
2019-08-21 09:26:23 -0300

    Adicionando Eslint e Prettier, p/ padronização

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req,res)=>{
-    return res.json({message: "Hello teste!"});
-})
+routes.get('/', (req, res) => res.json({ message: 'Hello teste!' }));
 
 export default routes;
